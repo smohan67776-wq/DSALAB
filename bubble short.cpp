debe470861a319dc27899f84b6400fdbcd267277
smohan67776-wq <smohan67776@gmail.com>
2026-02-22 21:40:55 -0800

    Add files via upload

---
+++ bubble short.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main () 
+{
+	int i,j, n,temp;
+	printf("enter number of elements:");
+	scanf("%d",&n);
+	int arr[n];
+	printf("enter %d elements:\n",n);
+	for(i=0;i<n;i++)
+	scanf("%d",&arr[i]);
+	for( i=0;i<n-1;i++)
+	{
+		for( j=0;j<n-1;j++)
+		{
+			if (arr[j]>arr[j+1])
+			{
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	printf("sorted array:\n");
+	for( i=0;i<n;i++)
+	printf("%d\t",arr[i]);
+	return 0;
+}
